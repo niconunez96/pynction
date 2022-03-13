3fb241af8270a14d81991a6f1b84dc90d0e4a65e
niconunez96 <nicolas110996@gmail.com>
2022-03-13 11:03:42 -0300

    Add stream factory methods

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -1,10 +1,10 @@
 import pytest
-from pynction.stream import Stream
+from pynction.stream import stream, stream_of
 
 
 class TestStream:
     def test_it_should_create_stream_from_list(self):
-        example_stream = Stream.of([1, 2, 3, 4])
+        example_stream = stream_of([1, 2, 3, 4])
 
         assert example_stream.to_list == [1, 2, 3, 4]
 
@@ -19,12 +19,12 @@ class TestStream:
     def test_it_should_create_stream_from_iterator(
         self, iterator_example, expected_value
     ):
-        example_stream = Stream.of(iterator_example)
+        example_stream = stream_of(iterator_example)
 
         assert example_stream.to_list == expected_value
 
     def test_it_should_create_stream_from_set(self):
-        example_stream = Stream.of({1, 2, 3, 4})
+        example_stream = stream_of({1, 2, 3, 4})
 
         assert example_stream.to_list == [1, 2, 3, 4]
 
@@ -34,49 +34,49 @@ class TestStream:
             yield 2
             yield 3
 
-        example_stream = Stream.of(example_generator())
+        example_stream = stream_of(example_generator())
 
         assert example_stream.to_list == [1, 2, 3]
 
     def test_it_should_create_stream_from_multiple_args(self):
-        example_stream = Stream(1, 2, 3, 4, 5)
+        example_stream = stream(1, 2, 3, 4, 5)
 
         assert example_stream.to_list == [1, 2, 3, 4, 5]
 
     def test_it_should_transform_elements_of_stream(self):
-        example_stream = Stream(1, 2, 3, 4, 5)
+        example_stream = stream(1, 2, 3, 4, 5)
 
         doubled_numbers = example_stream.map(lambda x: x * 2)
 
         assert doubled_numbers.to_list == [2, 4, 6, 8, 10]
 
     def test_it_should_filter_elements_of_stream(self):
-        example_stream = Stream(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        example_stream = stream(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
         even_numbers = example_stream.filter(lambda x: x % 2 == 0)
 
         assert even_numbers.to_list == [2, 4, 6, 8, 10]
 
     def test_it_should_transform_and_flatten_elements_of_stream(self):
-        example_stream = Stream(1, 2, 3, 4)
+        example_stream = stream(1, 2, 3, 4)
 
         doubled_and_tripled = example_stream.flat_map(lambda x: [x * 2, x * 3])
 
         assert doubled_and_tripled.to_list == [2, 3, 4, 6, 6, 9, 8, 12]
 
     def test_it_should_return_elements_until_condition_is_not_satisfied(self):
-        example_stream = Stream.of(range(1, 20))
+        example_stream = stream_of(range(1, 20))
 
         less_than_ten = example_stream.take_while(lambda x: x < 10)
 
         assert less_than_ten.to_list == [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     def test_it_should_convert_elements_to_set(self):
-        example_stream = Stream(1, 1, 1, 1, 1, 1, 1, 2, 2, 2)
+        example_stream = stream(1, 1, 1, 1, 1, 1, 1, 2, 2, 2)
 
         assert example_stream.to_set == {1, 2}
 
     def test_stream_should_be_iterable(self):
-        example_stream = Stream(1, 2, 3, 4)
+        example_stream = stream(1, 2, 3, 4)
 
         assert list(example_stream) == [1, 2, 3, 4]
