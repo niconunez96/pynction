add9ba0766839e84765322f4f1defbdfcd45cea5
niconunez96 <nicolas110996@gmail.com>
2022-10-02 13:09:46 -0300

    [PATCH] Ignore version file from pre-commit

--- pynction/version.py
+++ pynction/version.py
@@ -1,7 +1,2 @@
 __version_info__ = (0, 0, 4)
-__version__ = "-".join(
-    filter(
-        None,
-        [".".join(map(str, __version_info__[:3])), (__version_info__[3:] or [None])[0]],
-    )
-)
+__version__ = '-'.join(filter(None, ['.'.join(map(str, __version_info__[:3])), (__version_info__[3:] or [None])[0]]))
