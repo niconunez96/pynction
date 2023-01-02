b3e5ec370c03755aa445372c56c7756fd7c39423
niconunez96 <nicolas110996@gmail.com>
2023-01-02 00:01:49 -0300

    Use semantic-version gh and remove unnecessary config files

--- tasks.py
+++
@@ -1,7 +0,0 @@
-from invoke_release.tasks import *  # noqa: F403
-
-configure_release_parameters(  # noqa: F405
-    module_name="pynction",
-    display_name="pynction",
-    use_pull_request=True,
-)
